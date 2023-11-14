fa89cbcdcbc50cc8f865fec37b9fdebd1dd3aca0
musa-imperial <mohammed.choudhury19@imperial.ac.uk>
2023-11-14 20:07:50 +0000

    add kernels for corners

--- Reaction_diffusion/reaction_diffusion_kernels.h
+++ Reaction_diffusion/reaction_diffusion_kernels.h
@@ -1,3 +1,10 @@
+#define laplacianu(a, b, c, d) ((a+b+c-3*d))
+#define laplacianv(a, b, c, d) ((a+b+c-3*d))
+#define laplacian_corner(a, b, c) ((a+b-2*c))
+#define laplacianvc(a, b, c) ((a+b-2*c))
+#define f1(u, v) (epsilon*a*(1-u)*(u-(v+b)*div_a))*dt
+#define f2(v, u) (u*u*u-v)*dt
+
 void set_zero(ACC<double> &A) {
     A(0, 0) = 0.0;
 }
@@ -19,16 +26,73 @@ void apply_stencil(const ACC<double> &A, ACC<double> &Anew) {
 }
 
 
-void initcond_stencil(ACC<double> &A, const int *idx) {
-    double a = 0;
-    for (int m=0; m < 5; m++) {
-    a = a + 10*pow(-1, m)*4/(2*m+1)/(2*m+1)/pi/pi*sin((2*m+1)*pi*(idx[0]*dx));
-    
+void u_initcond_stencil(ACC<double> &A, const int *idx) {
+
+    if (idx[1] > Ly/2) {
+        A(0, 0) = 1;
     }
-    A(0, 0) = a;
+    else {
+        A(0, 0) = 0;
+    }
     
 }
 
-void laplacian(ACC<double> &A) {
+void v_initcond_stencil(ACC<double> &A, const int *idx) {
+
+    if (idx[0] < Lx/2) {
+        A(0, 0) = a/2;
+    }
+    else {
+        A(0, 0) = 0;
+    }
     
+}
+
+
+void bottomleft_u(const ACC<double> &A, ACC<double> &Anew, const ACC<double> &B) {
+    
+     Anew(0, 0) = (hmu1dt*laplacian_corner(A(1, 0), A(0, 1), A(0, 0))+f1(A(0, 0), B(0, 0)))+ A(0, 0);
+
+}
+
+void bottomleft_v(const ACC<double> &A, ACC<double> &Anew, const ACC<double> &B) {
+    
+     Anew(0, 0) = (hmu2dt*laplacian_corner(A(1, 0), A(0, 1), A(0, 0))+f2(A(0, 0), B(0, 0)))+ A(0, 0);
+
+}
+
+void topleft_u(const ACC<double> &A, ACC<double> &Anew, const ACC<double> &B) {
+    
+     Anew(0, 0) = (hmu1dt*laplacian_corner(A(1, 0), A(0, -1), A(0, 0))+f1(A(0, 0), B(0, 0)))+ A(0, 0);
+
+}
+
+void topleft_v(const ACC<double> &A, ACC<double> &Anew, const ACC<double> &B) {
+    
+     Anew(0, 0) = (hmu2dt*laplacian_corner(A(1, 0), A(0, -1), A(0, 0))+f2(A(0, 0), B(0, 0)))+ A(0, 0);
+
+}
+
+void bottomright_u(const ACC<double> &A, ACC<double> &Anew, const ACC<double> &B) {
+    
+     Anew(0, 0) = (hmu1dt*laplacian_corner(A(-1, 0), A(0, 1), A(0, 0))+f1(A(0, 0), B(0, 0)))+ A(0, 0);
+
+}
+
+void bottomright_v(const ACC<double> &A, ACC<double> &Anew, const ACC<double> &B) {
+    
+     Anew(0, 0) = (hmu2dt*laplacian_corner(A(-1, 0), A(0, 1), A(0, 0))+f2(A(0, 0), B(0, 0)))+ A(0, 0);
+
+}
+
+void topright_v(const ACC<double> &A, ACC<double> &Anew, const ACC<double> &B) {
+    
+     Anew(0, 0) = (hmu1dt*laplacian_corner(A(-1, 0), A(1, 0), A(0, 0))+f1(A(0, 0), B(0, 0)))+ A(0, 0);
+
+}
+
+void topright_u(const ACC<double> &A, ACC<double> &Anew, const ACC<double> &B) {
+    
+     Anew(0, 0) = (hmu2dt*laplacian_corner(A(-1, 0), A(1, 0), A(0, 0))+f2(A(0, 0), B(0, 0)))+ A(0, 0);
+
 }
