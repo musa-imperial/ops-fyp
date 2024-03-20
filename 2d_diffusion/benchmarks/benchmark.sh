33ea2e77978f65bae84b70cd479e521cb4432799
musa-imperial <mohammed.choudhury19@imperial.ac.uk>
2024-03-20 21:57:28 +0000

    better to use this as you can source

---
+++ 2d_diffusion/benchmarks/benchmark.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+max_thread_count=16
+
+compiler="gnu"
+
+export OPS_INSTALL_PATH=/home/musa/apps/OPS/ops
+
+if [ "$compiler" = "icx" ]; then
+    export OPS_COMPILER=icx
+    export INTEL_INSTALL_PATH=/home/musa/intel/oneapi
+
+    export PATH=$INTEL_INSTALL_PATH/compiler/latest/bin:$PATH
+    export MPICH_CC=icx
+    export MPICH_CXX=icpx
+
+    
+    source $INTEL_INSTALL_PATH/compiler/latest/env/vars.sh
+    source $INTEL_INSTALL_PATH/mpi/latest/env/vars.sh
+
+    export MPI_INSTALL_PATH=$INTEL_INSTALL_PATH/mpi/latest
+    export MPICPP=$MPI_INSTALL_PATH/bin/mpicxx
+
+    unset HDF5_INSTALL_PATH
+    export HDF5_INSTALL_PATH=/home/musa/apps/asl/install/build_hdf5/intel
+    export LD_LIBRARY_PATH=$HDF5_INSTALL_PATH/lib:$LD_LIBRARY_PATH
+    export PATH=$HDF5_INSTALL_PATH/bin:$PATH
+
+elif [ "$compiler" = "gnu" ]; then
+    export OPS_COMPILER=gnu
+    export HDF5_INSTALL_PATH=$HOME/apps/hdf5
+fi
+
+./removescript.sh
+make diffusion_seq
+
+
+echo -n " Runtime, Thread count" > benchmark_output.txt
+for (( t=1; t<=$max_thread_count; t++ )); do
+    export OMP_NUM_THREADS=$t
+    pi=$(./diffusion_seq)
+    echo -n "$pi, $t" >> benchmark_output.txt
+    echo "Runtime, $pi"
+    echo "Thread count: $t"
+done
