4bf76e2482768cde936a4e5c88fb908319fbb1a3
musa-imperial <mohammed.choudhury19@imperial.ac.uk>
2024-04-09 16:54:30 +0100

    added kernels and solution checking kernels idx[0]+1

--- 2d_diffusion/ops/diffusion_kernels.h
+++ 2d_diffusion/ops/diffusion_kernels.h
@@ -8,62 +8,23 @@ void copy(ACC<double> &A, const ACC<double> &Anew) {
     A(0, 0) = Anew(0, 0);
 }
 
-void u_initcond_stencil(ACC<double> &A, const int *idx) {
-
-    // if ((Lx/4 < idx[0] && Lx/4+a < idx[0]) && ((Lx/4 < idx[0] && Lx/4+a < idx[0])) && (Ly/4 < idx[1] && Ly/4+a < idx[1]) && ((Ly/4 < idx[1] && Ly/4+a < idx[1]))) {
-    if ((Lx/4 < idx[0] && Lx/4+a < idx[0]) && (Ly/4 < idx[1] && Ly/4+a < idx[1])) {
-        A(0, 0) = 1;
-    }
-    else {
-        A(0, 0) = 0;
-    }
-    
+void initial_condition(ACC<double> &A, const int *idx) {
+     
+    A(0,0) = 5*sin(M_PI*((idx[0]+1)*dx)/Lx)*sin(M_PI*((idx[1]+1)*dy)/Ly);
 }
-
-
-void bottomleft_u(const ACC<double> &A, ACC<double> &Anew) {
-    
-     Anew(0, 0) = A(0, 0)+(hnudt*(A(1, 0) + A(0, 1) - 2 * A(0, 0)));
-    
-}
-void topleft_u(const ACC<double> &A, ACC<double> &Anew) {
-    
-     Anew(0, 0) = A(0, 0)+(hnudt*(A(1, 0) + A(0, -1) - 2 * A(0, 0)));
-
-}
-void bottomright_u(const ACC<double> &A, ACC<double> &Anew) {
-    
-     Anew(0, 0) = A(0, 0)+(hnudt*(A(-1, 0) + A(0, 1) - 2 * A(0, 0)));
-
-}
-
-void topright_u(const ACC<double> &A, ACC<double> &Anew) {
-
-     Anew(0, 0) = A(0, 0)+(hnudt*(A(-1, 0) + A(0, -1) - 2 * A(0, 0)));
-}
-
 
 //top, bottom, left and right boundary condition kernels
-void left_bndcon_u(const ACC<double> &A, ACC<double> &Anew) {
-    Anew(0, 0) = A(0, 0)+hnudt*(A(1, 0) + A(0, 1) + A(0, -1) - 3 * A(0, 0));
+void boundary_condition(ACC<double> &Anew) {
+    Anew(0,0) = 0;
 }
-
-void right_bndcon_u(const ACC<double> &A, ACC<double> &Anew) {
-    Anew(0, 0) = A(0, 0)+hnudt*(A(-1, 0) + A(0, 1) + A(0, -1) - 3 * A(0, 0));
-}
-
-////////
-void top_bndcon_u(const ACC<double> &A, ACC<double> &Anew) {
-    Anew(0, 0) = A(0, 0)+hnudt*(A(1, 0) + A(-1, 0) + A(0, -1) - 3 * A(0, 0));
-}
-
-
-void bottom_bndcon_u(const ACC<double> &A, ACC<double> &Anew) {
-    Anew(0, 0) = A(0, 0)+hnudt*(A(1, 0) + A(-1, 0) + A(0, 1) - 3 * A(0, 0));
-}
-
 
 void interior_stencil_u(const ACC<double> &A, ACC<double> &Anew) {
     Anew(0, 0) = A(0, 0)+hnudt*(A(1, 0)+A(-1, 0)+A(0, 1)+A(0, -1)-4*A(0, 0));
 }
 
+void solution_check(const ACC<double> &A, const int *idx, double *l2error, double *max_error) {
+    double u = 5*exp(-nu*M_PI*M_PI*(1/Lx/Lx+1/Ly/Ly)*T)*sin(M_PI/Lx*(idx[0]+1)*dx)*sin(M_PI/Ly*(idx[1]+1)*dy);
+    *l2error = *l2error + sqrt(A(0,0)*A(0,0)-u*u);
+    *max_error = fmax(*max_error, fabs((A(0,0)-u)/u));
+    //std::cout << (idx[0]+1)*dx << ", "<< (idx[1]+1)*dy << ", " << (A(0,0)) << ", " << u << ", " << fabs((A(0,0)-u))/u << std::endl;
+}
