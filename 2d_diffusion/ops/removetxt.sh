b6163558308025c4565540ae562a0633cf72914d
musa-imperial <mohammed.choudhury19@imperial.ac.uk>
2024-05-22 03:37:12 +0100

    small change

--- 2d_diffusion/ops/removetxt.sh
+++ 2d_diffusion/ops/removetxt.sh
@@ -1,14 +1,14 @@
-# rm -rf CUDA
-# rm -rf HIP
-# rm -rf MPI_inline
-# rm -rf MPI_OpenMP
-# rm -rf OpenACC
-# rm -rf OpenCL
-# rm -rf OpenMP_offload
-# rm -rf SYCL
-# rm -rf .generated
-# rm -rf *_ops.cpp
-# rm -rf *_ops.c
+rm -rf CUDA
+rm -rf HIP
+rm -rf MPI_inline
+rm -rf MPI_OpenMP
+rm -rf OpenACC
+rm -rf OpenCL
+rm -rf OpenMP_offload
+rm -rf SYCL
+rm -rf .generated
+rm -rf *_ops.cpp
+rm -rf *_ops.c
 rm -rf *.txt
 rm -rf *.txt.*
 
